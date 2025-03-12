e1dfa6392286b393ed0bf4e3ad93d6d020fe2fc3
Monzer Abdullah <monzerabdullah.dev@gmail.com>
2025-03-12 02:32:07 +0200

    testing main functionalty is done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:sifat_salah/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,11 +16,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'sifat alsalah',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: background),
         useMaterial3: true,
         fontFamily: 'NotoKufiArabic',
       ),
-      home: const HomePage(),
+      home: Directionality(
+        textDirection: TextDirection.rtl,
+        child: const HomePage(),
+      ),
     );
   }
 }
@@ -52,7 +56,41 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.menu,
+            color: secondary,
+          ),
+        ),
         title: Text('صفة الصلاة'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.bookmark,
+                color: primary,
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.edit_note_outlined,
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.font_download,
+            ),
+          ),
+        ],
       ),
       body: SafeArea(
           child: book != null
@@ -62,8 +100,19 @@ class _HomePageState extends State<HomePage> {
                     final text = book['pages'][index]['text'];
                     return ListView(
                       children: [
-                        Text(
-                          text,
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 16,
+                            horizontal: 15,
+                          ),
+                          child: Text(
+                            text,
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                              fontSize: 18,
+                              height: 2,
+                            ),
+                          ),
                         ),
                       ],
                     );
